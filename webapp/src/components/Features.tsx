923e7fb452245921b4c761e08d9f27dd4529b92b
Vicwin13 <vnwimo13@gmail.com>
2025-12-10 02:43:57 +0100

    finishing up

--- webapp/src/components/Features.tsx
+++ webapp/src/components/Features.tsx
@@ -7,29 +7,39 @@ import { motion } from "motion/react";
 
 const features = [
     {
-        image: "/git-graph.svg",
+        image: "/open-source.svg",
         heading: "Open Source",
         desc: "Jagora offers a transparent and secure foundation for your onboarding, built by the community."
     },
        {
-        image: "/dessert.svg",
-        heading: "Customizable",
+        image: "/custom.svg",
+        heading: "Highly Customizable",
         desc: "Jagora provides full customization and configuration for all tour steps."
     },
        {
-        image: "/file-code.svg",
-        heading: "Easy to use",
+        image: "/easy.svg",
+        heading: "Awesome User-Experience",
         desc: "Easy tours for users, simple creation for developers."
     },
        {
-        image: "/messages-square.svg",
-        heading: "User Feedback",
-        desc: "Instant user feedback, constant improvement."
-    }
-]
-const Ripple = ({ size = 100 }) => {
-    return (
-        <>
+           image: "/feedback.svg",
+           heading: "User Feedback",
+           desc: "Instant user feedback, constant improvement."
+    },
+          {
+        image: "/onboarding.svg",
+        heading: "Onboarding Customers",
+        desc: "The onboarding of new users made easy"
+    },
+             {
+        image: "/growth.svg",
+        heading: "Business Growth",
+        desc: "A great boost to your business."
+    },
+    ]
+    const Ripple = ({ size = 100 }) => {
+        return (
+            <>
             {[0, 0.8, 1.7, 2.7, 3.7].map((delay, i) => (
                 <motion.div
                     key={i}
@@ -58,7 +68,7 @@ function FeatureCarousel() {
     const [currentIndex, setCurrentIndex] = useState(0);
     const length = features.length;
     const spacing = 180;
-    const scales = [0.3, 0.7, 1, 0.7, 0.3];
+    const scales = [0.4, 0.6, 1.2, 0.6, 0.4];
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -76,6 +86,8 @@ function FeatureCarousel() {
     }
 
     return (
+        <div>
+            <h2 className="text-3xl font-semibold p-4 pt-10">Features on Jagora</h2>
         <div className="relative w-full overflow-clip mb-15  h-screen flex justify-center items-center mx-auto">
             {getVisibleItems().map(({ index, position }) => (
                 <motion.div
@@ -83,13 +95,13 @@ function FeatureCarousel() {
                     className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 "
                     animate={{
             x: position * spacing,
-            scale: scales[position + 2],
+            scale: scales[position + 2 ],
             y: 0,
           }}
           transition={{ duration: 0.5 }}
                 >
                         <div>
-            {position === 0 && <Ripple size={1000} />}
+            {position === 0 && <Ripple size={800} />}
 
             <Image
               src={features[index].image}
@@ -97,33 +109,28 @@ function FeatureCarousel() {
               className={` object-cover rounded-full z-50 ${
                 position === 0 ? "border-4 bored-black" : ""
                                 }`}
-                            width={20}    
-                            height={20}
+                            width={160}    
+                            height={160}  
             />
           </div>
 
                 </motion.div>
             ))}
-<div className="mt-8 text-center z-20 ">
-        <div className="pt-3 absolute top-10 left-1/2 -translate-x-1/2">
-          {/* <p>{features[currentIndex].heading}</p> */}
+<div className="mt-2 text-center z-20 ">
+        <div className="pt-3 absolute top-15 left-1/2 -translate-x-1/2">
           <h1 className="text-2xl w-[20rem]  md:text-4xl lg:text-5xl lg:w-160 mx-auto">
             {features[currentIndex].heading}
           </h1>
         </div>
 
-        <div className="absolute w-[80%] mx-auto    bottom-1 left-1/2 -translate-x-1/2">
-          {/* <p className="mx-auto  w-full text-lg md:text-2xl md:w-[500px]  py-3.5 ">
-            {features[currentIndex].text2}
-          </p> */}
+        <div className="absolute w-[80%] mx-auto bottom-15 left-1/2 -translate-x-1/2">
           <div className="flex items-center justify-center bg-(--crm_green) border3 w-max border-2 md:w-fit mx-auto text-sm   sm:text-base px-2 sm:px-5 rounded-4xl py-1.5 gap-2 sm:gap-2.5">
             <p className="word-nowrap">{features[currentIndex].desc}</p>
-            {/* <div className="sm:w-1.5 sm:h-1.5 w-0.5 h-0.5 rounded-full bg-(--main-blue)"></div>
-            <p className="word-nowrap">{features[currentIndex].position}</p> */}
           </div>
         </div>
       </div>
         </div>
+    </div>
     )
     
 }
--- webapp/src/components/Navbar.tsx
+++ webapp/src/components/Navbar.tsx
@@ -1,18 +1,48 @@
+"use client";
+
+import React, { useEffect, useState } from 'react'
+
 import Link from 'next/link'
-import React from 'react'
+import { Menu } from 'lucide-react';
 
 export const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      window.removeEventListener("scroll",handleScroll)
+    }
+  }, []);
+
+  const navbarClasses = isScrolled
+    ? "fixed top-0 left-0 right-0 z-50 w-[90%] text-white mt-4 transition-all duration-300 bg-(--primary) backdrop-blur-lg shadow-lg "
+    : "relative z-50 transition-all duration-300 bg-(--neutral)";
+
+
   return (
-    <div className='w-[90%] mx-auto p-3  flex items-center justify-between mt-4 rounded'>
-          <div>Jagora</div>
-          <div className='flex justify-center items-center gap-2.5'>
-               <Link href="#">
+    <div className={` ${navbarClasses}  mx-auto p-3 px-15 bg-(--neutral)  flex items-center justify-between pt-4 rounded`}>
+          <div className="text-xl font-bold">Jagora</div>
+      <div className='flex justify-center items-center gap-4 nav'>
+         <Link href="/" className='hover:text-(--accent) hover:font-bold transition-all'>
+              Home
+              </Link>
+               <Link href="#" className='hover:text-(--accent) hover:font-bold transition-all'>
               Dashboard
               </Link>
-              <Link href="#">
+              <Link href="/about" className='hover:text-(--accent) hover:font-bold transition-all'>
               About Us
               </Link>
-            <Link href="#">
+            <Link href="#" className='hover:text-(--accent) hover:font-bold transition-all'>
               How it works
               </Link>
              
@@ -24,10 +54,13 @@ export const Navbar = () => {
               </button>
               </Link>
                <Link href="#">
-              <button className='text-white bg-indigo-600 tracking-tight px-4 py-2 rounded'>
+          <button className={`text-white demo-three tracking-tight px-4 py-2 bg-(--accent) rounded `}>
                 See a Demo
               </button>
-              </Link>
+        </Link>
+        <div>
+          <Menu className='menu' size={20} />
+        </div>
           </div>
     </div>
   )
